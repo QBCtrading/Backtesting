a99e3e1771f5a5c36f937c038ddcafdb8b74bc06
unknown <DINGHAITAO300@SDB.local>
2018-08-17 18:01:49 +0800

    EOD 20180817

--- Toolkit/updateClosing.py
+++ Toolkit/updateClosing.py
@@ -52,7 +52,7 @@ M1004132"
 
 l = pd.Series()
 start = ql.Date(8,8,2018)
-end = ql.Date(15,8,2018)
+end = ql.Date(16,8,2018)
 #end = getqlDate(datetime.date.today())
 
 schedule = ql.Schedule(start,end,ql.Period(ql.Daily),
